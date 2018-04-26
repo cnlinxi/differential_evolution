376cf1c1049ba40ab9c99325bcadd1714c16e151
Mengnan <cncmn@sina.cn>
2018-04-27 00:02:48 +0800

    add DE_DDQN

---
+++ DE_RL_s.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/4/26 10:17
+# @Author  : MengnanChen
+# @Site    : 
+# @File    : DE_RL_s.py
+# @Software: PyCharm Community Edition
